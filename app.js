c1d3727b27709ea7bc29791f12b08774e86b5369
ruskyvisky <numan_859@hotmail.com>
2025-09-07 00:05:42 +0300

    Fix : Fixed expressjs not running bug on the example code

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 
-const app = require('express')();
-const port = 3000;
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-// test
-// test2
